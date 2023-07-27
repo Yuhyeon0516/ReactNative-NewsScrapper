e1ea87386b1e087cc6b255a0c3fe24b9ec6437f0
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-27 21:31:34 +0900

    made getting News List and moving Detail Screen function.

--- src/screens/NewsDetailScreen.js
+++ src/screens/NewsDetailScreen.js
@@ -1,13 +1,29 @@
 import { View } from "react-native";
-import React from "react";
+import React, { useCallback } from "react";
 import { Header } from "../components/Header/Header";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import WebView from "react-native-webview";
+import { Spacer } from "../components/Spacer";
 
 export default function NewsDetailScreen() {
+  const navigation = useNavigation();
+  const routes = useRoute();
+  const onPressBack = useCallback(() => {
+    navigation.goBack();
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <Header>
-        <Header.Title title={"News Detail"} />
+        <Header.Group>
+          <Header.Icon iconName={"arrow-back"} onPress={onPressBack} />
+          <Spacer horizontal space={12} />
+          <View style={{ maxWidth: 200 }}>
+            <Header.Title title={routes.params.newsItem.title} />
+          </View>
+        </Header.Group>
       </Header>
+      <WebView style={{ flex: 1 }} source={{ uri: routes.params.newsItem.link }} />
     </View>
   );
 }
--- src/screens/NewsListScreen.js
+++ src/screens/NewsListScreen.js
@@ -1,13 +1,56 @@
-import { View } from "react-native";
-import React from "react";
+import { FlatList, View } from "react-native";
+import React, { useCallback, useState } from "react";
 import { Header } from "../components/Header/Header";
+import { useDispatch, useSelector } from "react-redux";
+import { Typography } from "../components/Typography";
+import { getNewsList } from "../action/news";
+import SingleLineInput from "../components/SingleLineInput";
+import { useNavigation } from "@react-navigation/native";
+import { CustomButton } from "../components/CustomButton";
 
 export default function NewsListScreen() {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const [query, setQuery] = useState("");
+  const newsList = useSelector((state) => state.news.newsList);
+
+  const onSubmitEditing = useCallback((query) => {
+    if (!query) return;
+    dispatch(getNewsList(query));
+  }, []);
+
+  const onPressListItem = useCallback((newsItem) => {
+    navigation.navigate("Detail", { newsItem });
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <Header>
         <Header.Title title={"New List"} />
       </Header>
+      <View style={{ flex: 1 }}>
+        <View style={{ paddingHorizontal: 24, paddingVertical: 12 }}>
+          <SingleLineInput value={query} onChangeText={setQuery} placeholder={"뉴스 검색어를 입력해주세요."} onSubmitEditing={() => onSubmitEditing(query)} />
+        </View>
+        <FlatList
+          style={{ flex: 1 }}
+          data={newsList}
+          renderItem={({ item }) => {
+            return (
+              <CustomButton onPress={() => onPressListItem(item)}>
+                <View style={{ flex: 1, paddingHorizontal: 20, paddingVertical: 8 }}>
+                  <Typography fontSize={24} numOfLines={1}>
+                    {item.title}
+                  </Typography>
+                  <Typography fontSize={16} numOfLines={2} color={"gray"}>
+                    {item.description}
+                  </Typography>
+                </View>
+              </CustomButton>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
