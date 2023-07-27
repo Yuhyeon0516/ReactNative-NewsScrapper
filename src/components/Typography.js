0058f3020f36e8c1de384e4e5abcf8e9734d120c
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-27 21:29:51 +0900

    add numberOfLines

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Text as RNText } from "react-native";
 
-export function Typography({ color, fontSize, children }) {
+export function Typography({ color, fontSize, children, numOfLines }) {
   return (
     <RNText
       style={{
         color: color,
         fontSize: fontSize,
       }}
+      numberOfLines={numOfLines}
     >
       {children}
     </RNText>
